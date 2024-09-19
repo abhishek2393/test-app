28195048597f8fb1234e680b102a2228ede9d62b
Abhishek <coopisthebestkick@gmail.com>
2024-09-20 03:42:22 +0530

    again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 
 import Image from "next/image";
-import { FETCH_ALL_PRODUCT , GET_STORE_BY_SLUG,GET_TESTIMONIALS_BY_STORENAME} from "@/graphqlQueries";
+import { FETCH_ALL_PRODUCT } from "@/graphqlQueries";
 import {client} from "../../sanity/lib/client";
 
 
@@ -18,45 +18,45 @@ type product={
 // fetch data with cache no-store
 const data = await client({query: FETCH_ALL_PRODUCT},);
 const productList = data.data.allProduct;
-const storeSlug = 'denver-stores'
-console.log(process.env.NODE_ENV )
-async function fetchStoreBySlug(slug:string) {
-  try {
-    const { data, errors } = await client({
-      query: GET_STORE_BY_SLUG,
-      variables: { slug }
-    });
-    if (errors) {
-      console.log('GraphQL Errors:', errors);
-      throw new Error('Failed to fetch store data');
-    }
-    // console.log('store',data)
-    await fetchTestimonialsByStoreName(storeSlug)
-    return data; // This should now work correctly
-  } catch (error) {
-    console.error('Error fetching store:', error);
-    throw error;
-  }
-}
+// const storeSlug = 'denver-stores'
+// console.log(process.env.NODE_ENV )
+// async function fetchStoreBySlug(slug:string) {
+//   try {
+//     const { data, errors } = await client({
+//       query: GET_STORE_BY_SLUG,
+//       variables: { slug }
+//     });
+//     if (errors) {
+//       console.log('GraphQL Errors:', errors);
+//       throw new Error('Failed to fetch store data');
+//     }
+//     // console.log('store',data)
+//     await fetchTestimonialsByStoreName(storeSlug)
+//     return data; // This should now work correctly
+//   } catch (error) {
+//     console.error('Error fetching store:', error);
+//     throw error;
+//   }
+// }
 
-async function fetchTestimonialsByStoreName(storeName:string) {
-  try {
-    const { data, errors } = await client({
-      query: GET_TESTIMONIALS_BY_STORENAME,
-      variables: { storeName }
-    });
-    // console.log('Raw response:', data)
-    if (errors) {
-      console.log('GraphQL Errors:', errors);
-      throw new Error('Failed to fetch testimonial data');
-    }
-    // console.log('testis',data)
-    return data; // This should now work correctly
-  } catch (error) {
-    console.error('Error fetching testis:', error);
-    throw error;
-  }
-}
+// async function fetchTestimonialsByStoreName(storeName:string) {
+//   try {
+//     const { data, errors } = await client({
+//       query: GET_TESTIMONIALS_BY_STORENAME,
+//       variables: { storeName }
+//     });
+//     // console.log('Raw response:', data)
+//     if (errors) {
+//       console.log('GraphQL Errors:', errors);
+//       throw new Error('Failed to fetch testimonial data');
+//     }
+//     // console.log('testis',data)
+//     return data; // This should now work correctly
+//   } catch (error) {
+//     console.error('Error fetching testis:', error);
+//     throw error;
+//   }
+// }
 
 // const storeData = await fetchStoreBySlug(storeSlug);
 // console.log('storeData',storeData)
